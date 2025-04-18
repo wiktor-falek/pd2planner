a7dd78fe347e5da94ac25f584e4a2a05624beaa8
wiktor-falek <falekwiktor@gmail.com>
2025-04-18 17:01:24 +0200

    server side build id validation

--- server/src/validators.ts
+++ server/src/validators.ts
@@ -2,12 +2,15 @@ import { z } from "zod";
 
 export const postCreateBuild = z.object({
 	body: z.object({
-		buildCode: z.string() /*.min().max()*/,
+		buildCode: z.string(), // TODO: base64 regex + custom build validation
 	}),
 });
 
 export const getBuildCode = z.object({
 	params: z.object({
-		buildId: z.string().length(12),
+		buildId: z
+			.string()
+			.length(12)
+			.regex(/^[a-zA-Z0-9]+$/),
 	}),
 });
