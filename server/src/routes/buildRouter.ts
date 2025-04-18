da684c7dfd3a31da0f3e456b6f2f1b874ac834a7
wiktor-falek <falekwiktor@gmail.com>
2025-04-19 01:41:10 +0200

    rate limiting

--- server/src/routes/buildRouter.ts
+++ server/src/routes/buildRouter.ts
@@ -2,9 +2,20 @@ import { Router } from "express";
 import { buildController } from "../instances.js";
 import { validate } from "../middlewares/validate.js";
 import { getBuildCode, postCreateBuild } from "../validators.js";
+import { createLimiter, readLimiter } from "../middlewares/limiters.js";
 
 export const router = Router();
 
-router.post("/", validate(postCreateBuild), buildController.createBuild.bind(buildController));
+router.post(
+	"/",
+	createLimiter,
+	validate(postCreateBuild),
+	buildController.createBuild.bind(buildController)
+);
 
-router.get("/:buildId", validate(getBuildCode), buildController.getBuildCode.bind(buildController));
+router.get(
+	"/:buildId",
+	readLimiter,
+	validate(getBuildCode),
+	buildController.getBuildCode.bind(buildController)
+);
